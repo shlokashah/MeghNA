bf8c1da10f0cf45de2f029f41f73ff024eec720c
shlokashah <shlokashah0603@gmail.com>
2020-08-02 02:35:22 +0530

    output images added

--- Mask RCNN/Test MaskRCNN/detect_cloud.py
+++ Mask RCNN/Test MaskRCNN/detect_cloud.py
@@ -5,7 +5,7 @@ import os
 DOWNLOAD H5 FILE FROM DRIVE AND PLACE IN THE SAME FOLDER. mask_rcnn_cloud_0010.h5
 '''
 def main():
-	os.system("python mask_rcnn_predict.py --weights tir.h5 --labels labels.txt --image images/satellite45.jpg ")
+	os.system("python mask_rcnn_predict.py --weights tir.h5 --labels labels.txt --image images/1.jpeg ")
 
 if __name__ == '__main__':
 	main()
