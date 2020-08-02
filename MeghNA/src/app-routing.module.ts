9883242beb4127a0ab958284abef3341370923d4
JimilProgGrammer <shahjimil35@gmail.com>
2020-08-02 20:11:10 +0530

    Added code to visualize both workflows and have an interactive playground for pixel-by-pixel feature extraction

--- MeghNA/src/app-routing.module.ts
+++ MeghNA/src/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })
   ],
   exports: [RouterModule]
 })
--- MeghNA/src/app/cloud-detect/cloud-detect.component.ts
+++ MeghNA/src/app/cloud-detect/cloud-detect.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild, ElementRef, ViewChildren, ChangeDetectorR
 import * as $ from 'jquery';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ApiCallerService } from '../services/api-caller.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cloud-detect',
@@ -16,48 +17,116 @@ export class CloudDetectComponent implements OnInit {
   detectionAlgorithm: string;
   imgPath: string;
   showWorkFlow1 = true;
+  cloudTifPath = '';
+  fileName = '';
+  maskOutput = '';
+  gifPath = '';
+  mse = '';
+  ssim = '';
 
   response = { };
+  data = [];
 
-  constructor(private spinner: NgxSpinnerService, private changeDetectorRef: ChangeDetectorRef, private _api: ApiCallerService) { }
+  constructor(private spinner: NgxSpinnerService
+    , private changeDetectorRef: ChangeDetectorRef
+    , private _api: ApiCallerService
+    , private router: Router) { }
 
   ngOnInit() {
     this._api.doGetRequest('/predictCloud').subscribe(res => {
       console.log(res);
-      let x = 0;
-      let y = 0;
       this.imgPath = this.baseUrl + res.image[1];
+      this.maskOutput = this.baseUrl + res.image[2];
+      this.gifPath = this.baseUrl + res.cnn[1];
       this.response = res;
-      this.response['kmeans'].forEach(element => {
+
+      this.mse = parseFloat(this.response['cnn'][2]).toFixed(4);
+      this.ssim = (parseFloat(this.response['cnn'][3]) * 100.0).toFixed(2) + '%';
+      
+      let i = 0;
+      while (i < 3) {
+        let element = this.response['kmeans'][i];
+        // tslint:disable-next-line: no-shadowed-variable
+        const list = [];
+        list.push(element[0]);
         element[1] = parseFloat(element[1]).toFixed(2);
+        list.push(element[1]);
         element[2] = parseFloat(element[2]).toFixed(2);
-      });
+        list.push(element[2]);
+        list.push(element[5]);
+
+        element = this.response['mpa'][i];
+        element[1] = parseFloat(element[1]).toFixed(2);
+        list.push(element[1]);
+        element[2] = parseFloat(element[2]).toFixed(2);
+        list.push(element[2]);
+        element[3] = parseFloat(element[3]).toFixed(2);
+        list.push(element[3]);
+
+        this.data.push(list);
+        i++;
+      }
+
+      console.log(this.data);
+      this.cloudTifPath = this.response['image'][0];
+      this.fileName = this.response['image'][4].split('.')[0];
+      console.log(this.fileName);
       this.drawCanvas(this.imgPath);
       const canvas = document.querySelector('canvas');
       const ctx = canvas.getContext('2d');
       // ctx.clearRect(0, 0, canvas.width, canvas.height);
-      canvas.onmousedown = function(e) {
-        const rect = canvas.getBoundingClientRect();
-        x = e.clientX - rect.left;
-        y = e.clientY - rect.top;
-        const marker = new Image();
-        marker.src = 'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png';
-        ctx.drawImage(marker, x, y, 25, 25);
-        return [x, y];
-      };
+      canvas.addEventListener('mousedown', this.placeMarker.bind(this));
+    });
+  }
+
+  placeMarker(e) {
+    const c = <HTMLCanvasElement>document.getElementById('myCanvas');
+    const ctx = c.getContext('2d');
+    const rect = c.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    const marker = new Image();
+    marker.src = 'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png';
+    ctx.drawImage(marker, x, y, 25, 25);
+    this.getCloudFeatures(y, x, ctx);
+  }
+
+  getCloudFeatures(x: number, y: number, ctx: CanvasRenderingContext2D) {
+    this._api.doPostRequest('/predictCloud', 
+      { 'posx': (x * (984 / 575)).toFixed(0), 'posy': (y * (1074 / 575)).toFixed(0), 'imageName': this.fileName }).subscribe(res => {
+        console.log(res);
+        ctx.font = '15px Calibri';
+        let markerText = '';
+        if (res['cloudy']) {
+          markerText = 'Mask: ' + res['mask'] + ', Type: ' + res['type'];
+        } else {
+          markerText = 'Not A Cloud!';
+        }
+        ctx.fillStyle = 'red';
+        ctx.fillText(markerText, x, y + 15);
     });
   }
 
   drawCanvas(imgPath) {
+    // if (document.getElementById('canvasDiv').childElementCount <= 0) {
+    //   document.getElementById('canvasDiv').append('<canvas id="myCanvas"></canvas>');
+    // }
     const c = <HTMLCanvasElement>document.getElementById('myCanvas');
     const ctx = c.getContext('2d');
     const img = new Image();
     img.onload = function() {
-      c.width = 500;
-      c.height = 500;
-      ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, 600, 600);
+      c.width = 575;
+      c.height = 575;
+      ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, 575, 575);
     };
     img.src = imgPath;
+  }
+
+  clearCanvas() {
+    const c = <HTMLCanvasElement>document.getElementById('myCanvas');
+    const ctx = c.getContext('2d');
+    ctx.clearRect(0, 0, c.width, c.height);
+    this.drawCanvas(this.imgPath);
   }
 
   getCursorPosition(canvas, event) {
@@ -117,13 +186,18 @@ export class CloudDetectComponent implements OnInit {
   //   this.dispatchHookOnW1();
   // }
 
-  selectDetectionAlgo() {
-    console.log(this.detectionAlgorithm);
-    this.imgPath = '../assets/img/' + this.detectionAlgorithm;
-  }
+  // selectDetectionAlgo() {
+  //   console.log(this.detectionAlgorithm);
+  //   this.imgPath = '../assets/img/' + this.detectionAlgorithm;
+  // }
 
   toggleWorkFlow() {
     this.showWorkFlow1 = !this.showWorkFlow1;
+    if (this.showWorkFlow1) {
+      this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+      this.router.onSameUrlNavigation = 'reload';
+      this.router.navigate(['/detect']);
+    }
   }
 
 }
